58053eeffa49b2734217fc3aa404784467afe5cd
Laxman Desai <lolopaw24@gmail.com>
2021-09-01 04:53:50 +0530

    👌 IMPROVE: Add local histogram equilization

--- HW2/assignment2/9/code/myHistogramEquilization.m
+++ HW2/assignment2/9/code/myHistogramEquilization.m
@@ -1,4 +1,4 @@
-function outImage = myHistogramEquilization(inImage, windowSize)
+function outImage = myHistogramEquilization(inImage, w)
     %*********************************************************************
     %
     % myHistogramEquilization - local histogram equilization
@@ -9,26 +9,47 @@ function outImage = myHistogramEquilization(inImage, windowSize)
     % Output:
     %   outImage = output image
     %
+    % Notes:
+    %   1. Window is assumed to inImagee inImage square of odd size w = 3, 5, 7, ...
+    %
     %*********************************************************************
 
-    [row, col] = size(inImage);
+    [m, n] = size(inImage);
 
-    % ? add padding of size windowSize to the image
-    % * window size = 2 * padding + 1
-    windowSize = floor(windowSize / 2);
-    inImage = padarray(inImage, windowSize);
+    % ? zero padding inImage to avoid boundary overflow problem
+    w = floor(w / 2);
+    inImage = padarray(inImage, [w, w]);
+
     outImage = zeros(size(inImage));
 
-    for i = 1:row
+    for i = w + 1:w + m
 
-        for j = 1:col
-            % imshow(inImage);
-            %
+        for j = w + 1:w + n
+            cdf = zeros(256, 1);
+
+            for x = -w:w
+
+                for y = -w:w
+                    pos = 1 + inImage(i + x, j + y);
+                    cdf(pos) = cdf(pos) + 1;
+                end
+
+            end
+
+            % ? cumulative distribution function
+            for l = 2:256
+                cdf(l) = cdf(l) + cdf(l - 1);
+            end
+
+            cdf = cdf / (2 * w + 1)^2;
+
+            pos = 1 + inImage(i, j);
+            outImage(i, j) = round(255.0 * cdf(pos));
         end
 
     end
 
-    wRow = windowSize(0); wCol = windowSize(1);
-    outImage = outImage(wRow + 1:wRow + row, wCol + 1:wCol + col);
+    % ? remove zero padding
+    outImage = outImage(w + 1:w + m, w + 1:w + n);
     outImage = uint8(outImage);
 end
