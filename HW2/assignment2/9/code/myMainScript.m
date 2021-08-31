2ce70f271e20fdc165d019bd157be293f72af562
Laxman Desai <lolopaw24@gmail.com>
2021-09-01 04:54:07 +0530

    👌 IMPROVE: Add Q9 script

--- HW2/assignment2/9/code/myMainScript.m
+++ HW2/assignment2/9/code/myMainScript.m
@@ -2,25 +2,43 @@
 
 tic;
 clc; clear;
+
 %% Your code here
+
+% ? report images save directory
+DIR = '../images';
 
 LC1 = imread('../data/LC1.png');
 LC2 = imread('../data/LC2.jpg');
 
-% Local histogram equalization
+%% Local histogram equalization
 
-windowSize = [7, 7; 31, 31; 51, 51; 71, 71];
+% windowSize = [7, 7; 31, 31; 51, 51; 71, 71];
+windowSize = [7, 31, 51, 71];
 
 for i = 1:size(windowSize, 1)
-    LC1_eq = myHistogramEquilization(LC1, windowSize(i, :));
-    LC2_eq = myHistogramEquilization(LC1, windowSize(i, :));
-    break
+    figure;
+    subplot(1, 2, 1), imshow(LC1);
+    title('Original image LC1');
+
+    LC1_eq = myHistogramEquilization(LC1, windowSize(i));
+    subplot(1, 2, 2), imshow(LC1_eq);
+    title(sprintf('Local histogram equalization with window size %d', windowSize(i)));
+
+    figure;
+    subplot(1, 2, 1), imshow(LC2);
+    title('Original image LC2');
+
+    LC2_eq = myHistogramEquilization(LC2, windowSize(i));
+    subplot(1, 2, 2), imshow(LC2_eq);
+    title(sprintf('Local histogram equalization with window size %d', windowSize(i)));
+
+    % save plot
+    fileName = sprintf('bilateral_%d.png', noise_sigma(i));
+    saveas(gcf, sprintf('../plots/myHistogramEqualization_%d.png', windowSize(i)));
+    exportgraphics(gcf, fullfile(DIR, fileName), 'Resolution', 300);
 end
 
-%
-
-%
-
 toc;
-pause;
-close all;
+% pause;
+% close all;
