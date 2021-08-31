4340d6ab8ab4dc50061f2b1451653fdb2ea0d5b3
Laxman Desai <lolopaw24@gmail.com>
2021-08-31 08:29:39 +0530

    📦 NEW: Add bilateral filter

--- HW2/assignment2/8/code/myMainScript.m
+++ HW2/assignment2/8/code/myMainScript.m
@@ -1,6 +1,55 @@
 %% MyMainScript
 
 tic;
+clc; clear;
 %% Your code here
+
+DIR = '../images';
+
+barbara = imread('../data/barbara256.png');
+subplot(1, 2, 1), imshow(barbara);
+title('Original image');
+
+%% Noisy image with sigma 5
+
+sigma_1 = 5;
+% ? Add Gaussian noise
+barbara_noisy_1 = double(barbara) + sigma_1 * randn(size(barbara));
+% ? round the pixel values, bring them in the range [0 255] and convert to double format
+barbara_noisy_1 = uint8(barbara_noisy_1);
+
+subplot(1, 2, 2), imshow(barbara_noisy_1);
+title('Noisy image (\sigma = 5)');
+
+%% Denoising using bilateral filtering
+
+% ? window size
+w = 5;
+% ? Spatial domain sigma
+sigma_s = [2, 0.1, 3];
+% ? Range domain sigma
+sigma_r = [2, 0.1, 15];
+
+for i = 1:3
+    % barbara_filtered = uint8(myBilateralFilter(barbara_noisy_1, sigma_s(i), sigma_r(i), w));
+
+    figure;
+    subplot(1, 2, 1), imshow(barbara_noisy_1);
+    title('Noisy image');
+    % subplot(1, 2, 2), imshow(barbara_filtered);
+    title(['Filtered image (\sigma_s = ', num2str(sigma_s(i)), ', \sigma_r = ', num2str(sigma_r(i)), ')']);
+    break;
+end
+
+% barbara_filtered_1a = myBilateralFilter(barbara_noisy, 5, 2, 2);
+% barbara_filtered_1b = myBilateralFilter(barbara_noisy, 5, 0.1, 0.1);
+% barbara_filtered_1c = myBilateralFilter(barbara_noisy, 5, 3, 15);
+
+% subplot(1, 2, 2), imshow(barbara_tst)
+% title('Noisy image')
+% barbara_noisy2 = barbara + 10 * randn(size(barbara));
+% barbara_filtered_2a = myBilateralFilter(barbara_noisy, 5, 2, 2);
+% barbara_filtered_2b = myBilateralFilter(barbara_noisy, 5, 0.1, 0.1);
+% barbara_filtered_2c = myBilateralFilter(barbara_noisy, 5, 3, 15);
 
 toc;
--- HW2/assignment2/8/code/mybilateralfilter.m
+++ HW2/assignment2/8/code/mybilateralfilter.m
@@ -1,35 +1,32 @@
-function output = myFun(input)
-    % myBilateralFilter - Applies Bilateral filter on the noisy image
-    %
-    % Syntax: output = myFun(input)
+function outputImage = myBilateralFilter(inputImage, sigma_s, sigma_r, w = 5)
+    % ? Input:
+    % * inputImage: the input image
+    % * sigma_s: the spatial standard deviation
+    % * sigma_r: the range standard deviation
+    % * w: the window size
+
+    % ? Output:
+    % * outputImage: The filtered image.
+
+    [row, col] = size(inputImage);
+    outputImage = zeros(row, col);
+
+    gaus = exp(-(X.^2 + Y.^2) / (2 * sigma_s^2));
+
+    for i = 1:row
+
+        for j = 1:col
+            % get the window (handle the overflow)
+            window = inputImage(max(i - w, 1):min(i + w, row), max(j - w):min(j + w, col));
+
+            scaledWindow = (exp(-1 * (window - inputImage(i, j)).^2 / (2 * sigma_r^2))) .* gaus((max(i - w, 1):min(i + w, row)) - i + w + 1, (max(j - w, 1):min(j + w, col)) - j + w + 1);
+
+            outputImage(i, j) = sum(scaledWindow(:) .* window(:)) / sum(scaledWindow(:));
+
+            % [X, Y] = meshgrid(-windowSize:windowSize, -windowSize:windowSize);
+            % outputImage(i, j) = exp(-1 * (inputImage(i, j) - inputImage(window)).^2 / (2 * sigma_r^2)) * exp(-1 * (window - inputImage(i, j)).^2 / (2 * sigma_s^2)) / weight;
+        end
+
+    end
 
 end
-
-% helper code
-% function myBilateralFilter(img, name)
-
-%     if (name == 'grass')
-%         grass(img);
-%         sigma_s = 0.8;
-%         count = 16;
-%     elseif (name == 'honey')
-%         honey(img);
-%         sigma_s = 0.95;
-%         count = 18;
-%     else
-%         barbara(img);
-%         sigma_s = 0.9;
-%         count = 17;
-%     end
-
-%     [X, Y] = meshgrid(-5:5, -5:5);
-%     G_space = exp(-(X.^2 + Y.^2) / (2 * sigma_s^2));
-%     imwrite(G_space(1:2 * 5 + 1, 1:2 * 5 + 1), "../images/Spatial_gaussian$" + sigma_s + ".png")
-%     figure(count)
-%     imagesc(G_space(1:2 * 5 + 1, 1:2 * 5 + 1));
-%     colormap(gray);
-%     colorbar
-%     axis on
-%     title("Spatial Gaussian for \sigma_s = " + sigma_s)
-
-% end
