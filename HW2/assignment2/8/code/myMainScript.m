815e26ec9cdfedee744437fd2ceab06d63a8fcfe
Laxman Desai <lolopaw24@gmail.com>
2021-09-01 09:33:36 +0530

    📦 NEW: Imp Q9 code

--- HW2/assignment2/8/code/myMainScript.m
+++ HW2/assignment2/8/code/myMainScript.m
@@ -9,6 +9,7 @@ clc; clear;
 DIR = '../images';
 
 barbara = imread('../data/barbara256.png');
+kodak = imread('../data/kodak24.png');
 % imshow(barbara);
 % title('Original image');
 
@@ -25,13 +26,15 @@ sigma_s = [2, 0.1, 3];
 sigma_r = [2, 0.1, 15];
 
 for i = 1:length(noise_sigma)
+    % * Barbara
+
     % ? Add Gaussian noise
     noisy_barbara = double(barbara) + noise_sigma(i) * randn(size(barbara));
     noisy_barbara = uint8(noisy_barbara);
 
-    figure(i);
+    figure;
     subplot(2, 2, 1), imshow(noisy_barbara);
-    title(sprintf('Noisy image (\\sigma = %d)', noise_sigma(i)));
+    title(sprintf('Noisy barbara (\\sigma = %d)', noise_sigma(i)));
 
     % ? Denoise using bilateral filtering
     for j = 1:length(sigma_s)
@@ -39,11 +42,33 @@ for i = 1:length(noise_sigma)
         denoised_barbara = uint8(denoised_barbara);
 
         subplot(2, 2, 1 + j), imshow(denoised_barbara);
-        title(sprintf('Filtered image (\\sigma_s = %.2f, \\sigma_r = %.2f)', sigma_s(j), sigma_r(j)));
+        title(sprintf('Filtered barbara (\\sigma_s: %.2f, \\sigma_r: %.2f)', sigma_s(j), sigma_r(j)));
     end
 
-    % fileName = sprintf('bilateral_%d.png', noise_sigma(i));
-    % exportgraphics(gcf, fullfile(DIR, fileName), 'Resolution', 300);
+    fileName = sprintf('barbara_bilateral_%d.png', noise_sigma(i));
+    exportgraphics(gcf, fullfile(DIR, fileName), 'Resolution', 300);
+
+    % * Kodak
+
+    % ? Add Gaussian noise
+    noisy_kodak = double(kodak) + noise_sigma(i) * randn(size(kodak));
+    noisy_kodak = uint8(noisy_kodak);
+
+    figure;
+    subplot(2, 2, 1), imshow(noisy_kodak);
+    title(sprintf('Noisy kodak (\\sigma: %d)', noise_sigma(i)));
+
+    % ? Denoise using bilateral filtering
+    for j = 1:length(sigma_s)
+        denoised_kodak = mybilateralfilter(noisy_kodak, sigma_s(j), sigma_r(j), w);
+        denoised_kodak = uint8(denoised_kodak);
+
+        subplot(2, 2, 1 + j), imshow(denoised_kodak);
+        title(sprintf('Filtered kodak (\\sigma_s: %.2f, \\sigma_r: %.2f)', sigma_s(j), sigma_r(j)));
+    end
+
+    fileName = sprintf('kodak_bilateral_%d.png', noise_sigma(i));
+    exportgraphics(gcf, fullfile(DIR, fileName), 'Resolution', 300);
 end
 
 toc;
