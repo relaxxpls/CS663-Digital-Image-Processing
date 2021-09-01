c5b198d95868e542ffd0718c65f13f97474eecf1
Laxman Desai <lolopaw24@gmail.com>
2021-09-01 05:36:40 +0530

    📦 NEW: Add global histogram equilization

--- HW2/assignment2/9/code/myMainScript.m
+++ HW2/assignment2/9/code/myMainScript.m
@@ -36,6 +36,27 @@ for i = 1:4
     exportgraphics(gcf, fullfile(DIR, fileName), 'Resolution', 300);
 end
 
+%% Global histogram equalization
+
+figure;
+subplot(2, 2, 1), imshow(LC1);
+title('LC1 - original');
+
+LC1_global_eq = histeq(LC1);
+subplot(2, 2, 2), imshow(LC1_global_eq);
+title('LC1 - Global histogram equalization');
+
+subplot(2, 2, 3), imshow(LC2);
+title('LC2 - original');
+
+LC2_global_eq = histeq(LC2);
+subplot(2, 2, 4), imshow(LC2_global_eq);
+title('LC2 - Global histogram equalization');
+
+% ? save plot
+fileName = 'GlobalHistogramEquilization.png';
+exportgraphics(gcf, fullfile(DIR, fileName), 'Resolution', 300);
+
 toc;
 % pause;
 close all;
