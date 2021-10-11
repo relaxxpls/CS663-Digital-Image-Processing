de285216f447ae4d0599bf90d84739cf2fa730c0
Laxman Desai <lolopaw24@gmail.com>
2021-10-11 08:49:17 +0530

    👌 IMPROVE: Save report images

--- HW3/assignment3/assignment3/2/code/myMainScript.m
+++ HW3/assignment3/assignment3/2/code/myMainScript.m
@@ -48,40 +48,48 @@ img11 = ifft2(ifftshift(fft_barbara .* H11));
 img11 = img11(m / 2 + 1:3 * m / 2, n / 2 + 1:3 * n / 2);
 figure;
 imshow(real(img11), []);
+exportgraphics(gcf, fullfile(DIR, 'ideal_40_image.png'), 'Resolution', 300);
 
 img12 = ifft2(ifftshift(fft_barbara .* H12));
 img12 = img12(m / 2 + 1:3 * m / 2, n / 2 + 1:3 * n / 2);
 figure;
 imshow(real(img12), []);
+exportgraphics(gcf, fullfile(DIR, 'ideal_80_image.png'), 'Resolution', 300);
 
 img21 = ifft2(ifftshift(fft_barbara .* H21));
 img21 = img21(m / 2 + 1:3 * m / 2, n / 2 + 1:3 * n / 2);
 figure;
 imshow(real(img21), []);
+exportgraphics(gcf, fullfile(DIR, 'gaussian_40_image.png'), 'Resolution', 300);
 
 img22 = ifft2(ifftshift(fft_barbara .* H22));
 img22 = img22(m / 2 + 1:3 * m / 2, n / 2 + 1:3 * n / 2);
 figure;
 imshow(real(img22), []);
+exportgraphics(gcf, fullfile(DIR, 'gaussian_80_image.png'), 'Resolution', 300);
 
 figure;
 imshow(H11, [min(abs(H11(:))) max(abs(H11(:)))]);
 colormap jet;
 colorbar;
+exportgraphics(gcf, fullfile(DIR, 'ideal_40_filter.png'), 'Resolution', 300);
 
 figure;
 imshow(H12, [min(abs(H12(:))) max(abs(H12(:)))]);
 colormap jet;
 colorbar;
+exportgraphics(gcf, fullfile(DIR, 'ideal_80_filter.png'), 'Resolution', 300);
 
 figure;
 imshow(H21, [min(abs(H21(:))) max(abs(H21(:)))]);
 colormap jet;
 colorbar;
+exportgraphics(gcf, fullfile(DIR, 'gaussian_40_filter.png'), 'Resolution', 300);
 
 figure;
 imshow(H22, [min(abs(H22(:))) max(abs(H22(:)))]);
 colormap jet;
 colorbar;
+exportgraphics(gcf, fullfile(DIR, 'gaussian_80_filter.png'), 'Resolution', 300);
 
 toc;
